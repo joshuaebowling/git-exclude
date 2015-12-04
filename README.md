ac396f2624b236facd113391bccec114dfee7156
Josh Bowling <joshuaebowling@gmail.com>
2015-12-04 09:06:42 -0500

    added template support and exluded .git contents for errors sake

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 #Git Exclude
-wip
+###Use 2 flags
+1. `--folder` = the folder you want to recursively read
+2. `--output` = the filename to which you want the contents written
+3. `--template` = the string template if you don't want `git rm --cached %s` to be your template
+
+###Run 
+`node ./app`
+### automatically excludes `.git` contents to prevent errors
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // modules
 var _, _str, flags, fs, util, wrench,
 // locals
-files, format, onRead, notToRead;
+files, format, onRead;
 
 _ = require('underscore');
 _str = require('underscore.string');
@@ -10,11 +10,17 @@ fs = require('fs');
 util = require('util');
 wrench = require('wrench');
 
-files = [];
-notToRead = [];
-format = 'git rm --cached %s';
+
+format = 'git rm --cached -r %s';
 onRead = function(fileNames) {
-	var strOutput = util.format(format, fileNames.join(' '));
+	var prunedFiles, strOutput;
+
+	prunedFiles = [];
+	_.each(fileNames, function(name, i) {
+	     var skip = _str.contains(name, '.git');
+	     if(!skip) prunedFiles.push(name);
+	});
+	strOutput = util.format(flags.get('template'), prunedFiles.join(' '));
 	fs.writeFile(flags.get('output'), strOutput, function() {
 		console.log('Complete');
 	});	
@@ -23,18 +29,16 @@ onRead = function(fileNames) {
 // define flags
 flags.defineString('folder', './', 'the folder to read');
 flags.defineString('output', '../exclude.txt', 'the output file');
-
+flags.defineString('template', format, 'the template for the output %s is position at which the value of the read op is inserted');
 flags.parse();
 
 //wrench logic
 
 // Recursively read directories contents
 files = wrench.readdirSyncRecursive(flags.get('folder'));
-onRead(files);
- //function(error, curFiles) {
-//	onRead(curFiles);    
-//});
 
+//output the goodies
+onRead(files);
 
 
 
